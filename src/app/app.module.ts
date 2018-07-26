fa73e68770b8a7e0a1b1a1c26961ee300074a2ec
borjalagarejo <borjalagarejo@gmail.com>
2018-07-26 09:13:00 +0200

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { BuscarComponent } from './componentes/buscar/buscar.component';
+import { ArtistaComponent } from './componentes/artista/artista.component';
+import { NavbarComponent } from './componentes/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BuscarComponent,
+    ArtistaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
