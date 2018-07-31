56c15d5dddc956bbb8c29b7e70e12da0916a4f4c
borjalagarejo <borjalagarejo@gmail.com>
2018-07-31 14:53:08 +0200

    pruebas api

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -8,15 +8,29 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
-  todo: any = {};
+  todo: any = [];
   cargado = false;
+  datos: any = [];
+
+
+  urlPeticion = 'https://foro.gameflix.es/api/index.php?/forums/topics&key=994f7a52ca1f11e4443d93677be5d0f7&perPage=1000&forums=90';
   constructor(private http: HttpClient) {
-    // tslint:disable-next-line:max-line-length
-    this.http.get('https://foro.gameflix.es/api/index.php?/core/members&key=994f7a52ca1f11e4443d93677be5d0f7').subscribe((respuesta: any) => {
-      this.todo = respuesta;
+    this.http.get(this.urlPeticion).subscribe((respuesta: any) => {
+      for (let i = 0; i < respuesta.results.length; i++) {
+        if (respuesta.results[i].prefix == null) {
+          const objeto = {
+            autor: respuesta.results[i].firstPost.author.name,
+            estado: respuesta.results[i].prefix,
+            texto: respuesta.results[i].firstPost.content
+          };
+          this.datos.push(objeto);
+        }
+
+      }
       this.cargado = true;
     });
   }
+
 
   ngOnInit() {
   }
